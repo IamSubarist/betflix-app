8ca86ffdb670eb8b2e1c5a1e87957024ce5c199b
IamSubarist <optimistroma927@gmail.com>
2025-02-06 18:52:07 +0300

    Проинциализировали реакт, написали чуть чуть логики

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { currentQuerySlice } from '../features/currentQuerySlice';
+import { kinopoiskApi } from '../services/kinopoiskApi';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [kinopoiskApi.reducerPath]: kinopoiskApi.reducer,
+    currentQuery: currentQuerySlice,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(kinopoiskApi.middleware),
 });
--- src/components/pages/MoviesListTop/MoviesListTop.jsx
+++ src/components/pages/MoviesListTop/MoviesListTop.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useGetFilmsTopQuery } from '../../../services/kinopoiskApi';
+import { TOP_LIST } from '../../../constants';
+import { useLocation } from 'react-router-dom';
 
 export const MoviesListTop = () => {
+  const location = useLocation();
+  const [page, setPage] = useState(1);
+  const movieType = TOP_LIST.find(el => el.url === location.pathname);
+
+  const { data, error, isLoading } = useGetFilmsTopQuery({
+    type: movieType.value,
+    page,
+  });
+  console.log(data, error, isLoading);
+
   return <div>MoviesListTop</div>;
 };
