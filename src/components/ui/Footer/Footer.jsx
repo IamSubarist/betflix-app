2a9b98c4d64d378ff72f66a9a932fdf49e8ffd77
IamSubarist <optimistroma927@gmail.com>
2025-02-06 13:48:21 +0300

    Initializing Footer

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Stack component="footer" sx={{ paddingTop: 4, paddingBottom: 4 }}>
+      <Typography variant="body2" color="text.secondary">
+        &copy; {new Date().getFullYear()} &laquo;BetFlix&raquo; 18+
+        <br />
+        Данный сайт создан исключительно в обучающих целях.
+        <br />
+        Все права принадлежат правообладателям.
+      </Typography>
+      <Typography variant="h4" color="primary.main">
+        BetFlix
+      </Typography>
+    </Stack>
+  );
+};
