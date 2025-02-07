1ef01a91359bf70f63b204b72cfa4f79ba514e99
IamSubarist <optimistroma927@gmail.com>
2025-02-07 17:51:53 +0300

    Первая генерация слайдера на главной странице

--- src/hooks/useMoviesQuery.jsx
+++ src/hooks/useMoviesQuery.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { useGetFilmsTopQuery } from '../services/kinopoiskApi';
+import {
+  useGetFilmsQuery,
+  useGetFilmsTopQuery,
+} from '../services/kinopoiskApi';
 import { TOP_LIST } from '../constants';
 import { useSelector } from 'react-redux';
 
 export const useMoviesQuery = () => {
-  const { page } = useSelector(state => state.currentQuerySlice);
+  const { countries, order, year, page } = useSelector(
+    state => state.currentQuerySlice,
+  );
 
   const responsePopular = useGetFilmsTopQuery({
     type: TOP_LIST[0].value,
@@ -16,11 +21,54 @@ export const useMoviesQuery = () => {
     page,
   });
 
-  const responseFilms;
-  const responseSerials;
-  const responseCartoons;
-  const isLoading = responsePopular.isFetching || responseBest.isFetching || responseFilms.isFetching || responseSerials.isFetching || responseCartoons.isFetching
+  const responseFilms = useGetFilmsQuery({
+    countries,
+    genreId: '1',
+    order,
+    type: 'FILM',
+    year,
+    page,
+  });
 
+  const responseSerials = useGetFilmsQuery({
+    countries,
+    genreId: '1',
+    order,
+    type: 'TV_SERIES',
+    year,
+    page,
+  });
 
-  return {};
+  const responseCartoons = useGetFilmsQuery({
+    countries,
+    genreId: '18',
+    order,
+    type: 'FILM',
+    year,
+    page,
+  });
+
+  const isLoading =
+    responsePopular.isFetching ||
+    responseBest.isFetching ||
+    responseFilms.isFetching ||
+    responseSerials.isFetching ||
+    responseCartoons.isFetching;
+
+  const hasError =
+    responsePopular.isFetching ||
+    responseBest.isFetching ||
+    responseFilms.isFetching ||
+    responseSerials.isFetching ||
+    responseCartoons.isFetching;
+
+  return {
+    isLoading,
+    hasError,
+    responsePopular,
+    responseBest,
+    responseFilms,
+    responseSerials,
+    responseCartoons,
+  };
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { App } from './components/App.jsx';
 import { CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
+import 'bear-react-carousel/dist/index.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
